20fddba435ccf65871c1c4a4e0792c3c7d2130aa
Katsumi Noguchi <noguchi@KatsuminoMacBook-Pro.local>
2019-07-13 13:23:11 +0900

    logfiles move to logs directory

--- src/log.h
+++ src/log.h
@@ -3,6 +3,7 @@
 #include <M5Stack.h>
 #include "Rtc.h"
 
+#define LOG_DIR ("/logs")
 class Log
 {
 private:
@@ -17,8 +18,10 @@ public:
     bool flgSD = SD.begin(TFCARD_CS_PIN, SPI, 40000000);
     if (flgSD)
     {
+      if (!SD.exists(LOG_DIR))
+        SD.mkdir(LOG_DIR);
       tm *t = _rtc->getTm();
-      snprintf(_buf, sizeof(_buf), "/log_%04d%02d%02d.txt", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
+      snprintf(_buf, sizeof(_buf), "%s/log_%04d%02d%02d.txt", LOG_DIR, t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
       File f = SD.open(_buf, FILE_APPEND);
       f.printf("%04d/%02d/%02d %02d:%02d:%02d %s", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec, msg);
       f.println();
